65e7a26544b7d76eb16988c9b2d751e1995686bb
Mohammed Maher <112744223+MohammedMaher177@users.noreply.github.com>
2023-04-28 02:49:38 +0200

    Add files via upload

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -5,37 +5,13 @@ import { Link } from "react-router-dom";
 import { cartContext } from "../../Context/CartContext";
 import { Toaster, toast } from "react-hot-toast";
 import Products from "../Products/Products";
+import Test from '../Test/Test'
 export default function Home() {
   const [products, setproducts] = useState([]);
-  const [isLoading, setisLoading] = useState(true);
+  const [isLoading, setisLoading] = useState(false);
   let { addToCart } = useContext(cartContext);
-  function addProductToCart(id) {
-    let response = addToCart(id);
-
-    toast.promise(
-      response,
-      {
-        loading: "Loading, Please Wait",
-        success: "Added Success",
-        error: "Error when fetching",
-      },
-      {
-        position: "top-right",
-      }
-    );
-  }
-  async function getProducts() {
-    return await axios
-      .get(`https://route-ecommerce.onrender.com/api/v1/products`)
-      .then((res) => {
-        setproducts(res.data.data);
-        setisLoading(false);
-      });
-  }
-
-  useEffect(() => {
-    getProducts();
-  }, []);
+  
+  
 
   return (
     <>
@@ -44,12 +20,12 @@ export default function Home() {
           <i className="fa-solid text-main fa-spinner fa-spin  fs-1"></i>
         </div>
       )}
-      <Toaster />
+      
       <div className="row py-5">
-        <div className="col-4">
+        <div className="col-lg-4">
           <MainSlider />
         </div>
-
+        <Toaster />
         <Products />
       </div>
     </>
